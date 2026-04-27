348d7069890bad4dd6aefb14605f3e92317bebb7
Chisato Kamakura <chisatokamakura@Chisatos-MacBook-Pro-612.local>
2026-04-27 11:26:45 -0700

    add requirements

---
+++ main.py
@@ -0,0 +1,17 @@
+'''
+Starts a hello world observer.
+'''
+
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+import uvicorn
+
+app = FastAPI()
+
+@app.get('/', response_class=HTMLResponse)
+async def index(request: Request):
+    return 'hello <b>world</b>'
+
+if __name__ == '__main__':
+    uvicorn.run("main:app", host="127.0.0.1", port=8080, reload=True)
+

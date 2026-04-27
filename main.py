3c5c8a95d88f243399a8e801727e3d2454f9185a
Chisato Kamakura <chisatokamakura@Chisatos-MacBook-Pro-612.local>
2026-04-27 11:41:14 -0700

    make changes

--- main.py
+++ main.py
@@ -4,13 +4,22 @@ Starts a hello world observer.
 
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 import uvicorn
 
 app = FastAPI()
+templates = Jinja2Templates(directory='templates')
 
+# Internal Server Error: 
+# always means a python error inside of the function that corresponds to
+# the route or "page" you were connecting to in Firefox
 @app.get('/', response_class=HTMLResponse)
 async def index(request: Request):
-    return 'hello <b>world</b>'
+    return templates.TemplateResponse(
+        request=request,
+        name='index.html'
+    )
+    return '<i>hello</i> <b>world</b>'
 
 if __name__ == '__main__':
     uvicorn.run("main:app", host="127.0.0.1", port=8080, reload=True)

6a67c7a0f2925ca7f4a84b801a7d0106b74e6f7d
Chisato Kamakura <chisatokamakura@Chisatos-MacBook-Pro-707.local>
2026-05-01 15:53:27 -0700

    create fastapi app

---
+++ README.md
@@ -0,0 +1,52 @@
+# Twitter Clone
+
+This project functions similarly to Twitter, where users can create accounts, write messages, and view a real-time user feed.
+
+This is a screenshot of the working home page.
+![Home Page](homepage.png)
+
+## Features
+- Create a user account
+- Log in and out 
+- Write and post a message 
+- View all messages posted, sorted by most recent 
+- Displays username, age, and time posted
+- Styled with CSS and contains static images 
+
+## How to Run the Tool: 
+
+First, set up the virtual environment (if needed) and install dependencies:
+```bash
+python3 -m venv venv
+source venv/bin/activate
+pip install fastapi uvicorn jinja2
+```
+
+Then, create a database:
+```bash
+python3 db_create.py
+```
+
+Start the server: 
+```bash
+python3 main.py
+```
+
+Open link in browser:
+```bash
+http://127.0.0.1:8080
+```
+
+## Database Design 
+
+### Users Table 
+- id (primary key)
+- username
+- password
+- age
+
+## Messages Table 
+- id integer (primary key)
+- sender_id (foreign key → users.id)
+- message
+- created_at (timestamp)
--- main.py
+++ main.py
@@ -4,11 +4,14 @@ Starts a hello world observer.
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 import uvicorn
 import sqlite3
 
 app = FastAPI()
 templates = Jinja2Templates(directory='templates')
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 # Internal Server Error: 
 # always means a python error inside of the function that corresponds to
@@ -20,29 +23,73 @@ async def index(request: Request):
     # extract username from database
     con = sqlite3.connect('twitter_clone.db')
     cur = con.cursor()
-    sql = """
-    SELECT username FROM users WHERE id=1;
-    """
-    cur.execute(sql)
-    for row in cur.fetchall():
-        username = row[0]
-        
-    print('row[0]=', row[0])
 
-    # return response
+    sql = """
+    SELECT users.username, users.age, messages.message, messages.created_at
+    FROM messages
+    JOIN users ON messages.sender_id = users.id
+    ORDER BY messages.created_at DESC;
+    """
+
+    cur.execute(sql)
+
+    messages = []
+    for row in cur.fetchall():
+        message_dict = {
+            'username': row[0],
+            'age': row[1],
+            'message': row[2],
+            'created_at': row[3],
+        }
+        messages.append(message_dict)
+
+    con.close()
+
     return templates.TemplateResponse(
         request=request,
         name='index.html',
         context={
             'is_logged_in': is_logged_in,
-            'username': username,
+            'messages': messages,
         }
     )
-    return '<i>hello</i> <b>world</b>'
 
 @app.get('/login', response_class=HTMLResponse)
 async def login(request: Request):
+    is_logged_in = False
+    return templates.TemplateResponse(
+        request=request,
+        name='login.html',
+        context={
+            'is_logged_in': is_logged_in,
+        }
+    )
+
+@app.get('/logout', response_class=HTMLResponse)
+async def login(request: Request):
+    is_logged_in = False
+    return templates.TemplateResponse(
+        request=request,
+        name='login.html',
+        context={
+            'is_logged_in': is_logged_in,
+        }
+    )
+
+@app.get('/create_message', response_class=HTMLResponse)
+async def login(request: Request):
     is_logged_in = True
+    return templates.TemplateResponse(
+        request=request,
+        name='login.html',
+        context={
+            'is_logged_in': is_logged_in,
+        }
+    )
+
+@app.get('/create_user', response_class=HTMLResponse)
+async def login(request: Request):
+    is_logged_in = False
     return templates.TemplateResponse(
         request=request,
         name='login.html',
